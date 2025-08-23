e940618aff81033769e18b535efb6c1d8633996d
Charllcrot <samuelrmillon@gmail.com>
2025-08-23 12:08:13 +0200

    Added the 0.2 version

---
+++ Page Calculator/Source_0.2/main.js
@@ -0,0 +1,33 @@
+//const Math = require("math")
+const contenedor = document.getElementById("botones");
+const btn = contenedor.querySelector("#Calcular");
+const input_a = document.getElementById("a");
+const input_b = document.getElementById("b");
+const selecter = document.getElementById("select");
+const display = document.getElementById("pantalla");
+
+
+const orders = {
+    sumar: (x, y) => (x + y),
+    restar: (x, y) => (x - y),
+    multiplicar: (x, y) => (x * y),
+    dividir: function(x, y){
+        if (y === 0)
+            return 0;
+        return x/y
+    },
+    elevar: (x, y) => (x**y),
+    root: function(x, y){
+        if (y < 0)
+            return 0
+        return Math.sqrt(x, y);
+    }
+}
+
+
+
+btn.addEventListener("click", function (event){
+    display.textContent = orders[selecter.value](Number(input_a.value), Number(input_b.value));
+})
+
+
